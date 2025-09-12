0777f44c22b3b57b271543779b2a38edc2adbc20
Faro123-sudo <137581692+Faro123-sudo@users.noreply.github.com>
2025-09-12 13:53:58 +0100

    Updated

--- nextstep-navigator/src/components/Contact.jsx
+++ nextstep-navigator/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaCommentDots } from "react-icons/fa";
 import "./ContactUs.css";
